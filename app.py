e54c7c7c22c774a503bfcb866008a86e6c22e776
Zanderz17 <sanders.chancan@utec.edu.pe>
2023-10-25 20:26:48 -0500

    /search_city

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+import xml.etree.ElementTree as ET
+
 
 app = Flask(__name__)
 
@@ -6,5 +9,41 @@ app = Flask(__name__)
 def hello_world():
   return 'Hi!'
 
+# Ruta para buscar la ubicación de una ciudad en la API de Nominatim
+@app.route('/search_city')
+def search_city():
+  print("ga")
+  # Obtén el valor del parámetro 'city' de la URL
+  city = request.args.get('city')
+
+  # Verifica si se proporcionó el parámetro 'city'
+  if not city:
+    return jsonify({'error': 'El parámetro "city" es obligatorio'}), 400
+
+  # URL de la API de Nominatim
+  api_url = 'https://nominatim.openstreetmap.org/search'
+
+  # Parámetros de la solicitud
+  params = {
+    'q': city,
+    'format': 'json'
+  }
+
+  # Realiza la solicitud a la API de Nominatim
+  response = requests.get(api_url, params=params)
+  # Verifica si la solicitud fue exitosa
+  if response.status_code == 200:
+    data = response.json()
+    result = {
+    "lat": data[0]['lat'],
+    "lon": data[0]['lon']
+    }   
+
+    return jsonify(result)
+  else:
+    return jsonify({'error': 'Error al realizar la solicitud a la API'}), 500
+
+
+
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
