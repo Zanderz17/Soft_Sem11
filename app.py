4ddd17c165885493de6d5e17c0e1ec466b853db8
Br4yanGC <brayan20160113@gmail.com>
2023-10-25 20:57:10 -0500

    tomorrow and next 7d temperature

--- app.py
+++ app.py
@@ -9,15 +9,10 @@ app = Flask(__name__)
 def hello_world():
   return 'Hi!'
 
-@app.route('/get_temperature', methods=['GET'])
-def get_temperature():
+@app.route('/get_tomorrow_temperature', methods=['GET'])
+def get_tomorrow_temperature():
     latitude = request.args.get('latitude')
     longitude = request.args.get('longitude')
-    time = request.args.get('time')  # 'morning', 'afternoon', or 'night'
-
-    print(latitude)
-    print(longitude)
-    print(time)
 
     # Perform the internal request to Open Meteo API using latitude and longitude
     open_meteo_url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&forecast_days=2&hourly=temperature_2m&timezone=PST'
@@ -28,30 +23,42 @@ def get_temperature():
         temperatures = data['hourly']['temperature_2m']
         timestamp = data['hourly']['time']
 
-        # Define time ranges
-        morning_range = (7, 12)
-        afternoon_range = (12, 18)
-        night_range = (18, 24)
+        # Extract temperatures based on the selected time range
+        temperature_data = []
+        for i, hour in enumerate(timestamp):
+            if (i >= 24):
+              hour = int(hour.split('T')[1].split(':')[0])
+              temperature_data.append({'time': hour, 'temperature_2m': temperatures[i]})
+        return jsonify(temperature_data)
+    else:
+        return jsonify({'error': 'Failed to fetch data from Open Meteo API'}), response.status_code
 
-        if time == 'morning':
-            start_hour, end_hour = morning_range
-        elif time == 'afternoon':
-            start_hour, end_hour = afternoon_range
-        elif time == 'night':
-            start_hour, end_hour = night_range
-        else:
-            return jsonify({'error': 'Invalid time parameter'}), 400
+@app.route('/get_next_7d_temperature', methods=['GET'])
+def get_next_7d_temperature():
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+
+    # Perform the internal request to Open Meteo API using latitude and longitude
+    open_meteo_url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&forecast_days=8&hourly=temperature_2m&timezone=PST'
+    response = requests.get(open_meteo_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        temperatures = data['hourly']['temperature_2m']
+        timestamp = data['hourly']['time']
 
         # Extract temperatures based on the selected time range
         temperature_data = []
         for i, hour in enumerate(timestamp):
-            hour = int(hour.split('T')[1].split(':')[0])
-            if start_hour <= hour < end_hour:
-                temperature_data.append({'time': hour, 'temperature_2m': temperatures[i]})
+            if (24 <= i < 24*8):
+                time = hour.split('T')[1].split(':')[0]
+                date = hour.split('T')[0]
+                temperature_data.append({'date': date, 'time': time, 'temperature_2m': temperatures[i]})
 
         return jsonify(temperature_data)
     else:
         return jsonify({'error': 'Failed to fetch data from Open Meteo API'}), response.status_code
+
 
 # Ruta para buscar la ubicación de una ciudad en la API de Nominatim
 @app.route('/search_city')
